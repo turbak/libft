292a76114f1d2b74dd8adb63cd324a0c4f8231ed
Cave Uranus <cauranus@at-c1.21-school.ru>
2019-09-11 19:20:01 +0300

    norm

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cauranus <cauranus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/04 13:52:28 by cauranus          #+#    #+#             */
-/*   Updated: 2019/09/11 14:06:32 by cauranus         ###   ########.fr       */
+/*   Updated: 2019/09/11 19:19:44 by cauranus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,8 +82,6 @@ typedef struct		s_list
 	size_t			content_size;
 	struct s_list	*next;
 }					t_list;
-
-
 
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
